c5bda6ebf7589c9f5e3be58105e50444d9e09286
Baptiste Lefort <71429321+blefo@users.noreply.github.com>
2021-06-23 13:37:40 +0200

    Update ParsJSON.scala

--- ParsJSON.scala
+++ ParsJSON.scala
@@ -2,60 +2,62 @@ import net.liftweb.json.DefaultFormats
 import net.liftweb.json._
 
 object ParseJsonArray extends App {
-    implicit val formats = DefaultFormats
-
-    // a JSON string that represents a list of EmailAccount instances
-    val jsonString ="""
-    {
-      "accounts": [
-    { "emailAccount": {
-      "accountName": "YMail",
-      "username": "USERNAME",
-      "password": "PASSWORD",
-      "url": "imap.yahoo.com",
-      "minutesBetweenChecks": 1,
-      "usersOfInterest": ["barney", "betty", "wilma"],
-      "type" : "String"
-    }},
-    { "emailAccount": {
-      "accountName": "Gmail",
-      "username": "USER",
-      "password": "PASS",
-      "url": "imap.gmail.com",
-      "minutesBetweenChecks": 1,
-      "usersOfInterest": ["pebbles", "bam-bam"],
-      "type" : "String"
-    }}]}
-    """
-
-    def parsingJSON(json:String) : List[Any] = {
-      // json is a JValue instance
-      val json = parse(jsonString)
-      val elements = (json).children
-      return(elements)
+  implicit val formats = DefaultFormats
+// a JSON string that represents a list of EmailAccount instances
+  val jsonString = """
+{
+  "$id": "https://example.com/geographical-location.schema.json",
+  "$schema": "https://json-schema.org/draft/2020-12/schema",
+  "title": "Longitude and Latitude Values",
+  "description": "A geographical coordinate.",
+  "required": [ "latitude", "longitude" ],
+  "type": "object",
+  "properties": {
+    "latitude": {
+      "type": "number",
+      "minimum": -90,
+      "maximum": 90
+    },
+    "longitude": {
+      "type": "number",
+      "minimum": -180,
+      "maximum": 180
     }
-  
-    println(parsingJSON(jsonString))
-  
-    def browseList(listData : Any) : Any = {
-
-      if(listData.isInstanceOf[List[Any]] == true){
-        return(listData)
-      }else if (listData.isInstanceOf[JArray] == true){
-        return(listData)
-      }else if (listData.isInstanceOf[JObject] == true){
-        return(browseList(listData(0)))
-      }else if (listData.isInstanceOf[JField] == true){
-        return(browseList(listData(0)))
-      }else if (listData.isInstanceOf[JString] == true){
-        return(browseList(listData(0)))
-      }
-      
-    }
-  
-    println(browseList(parsingJSON(jsonString)))
-  
-    
-  
-  
+  }
 }
+"""
+
+  val json = parse(jsonString) // on doit itt sur les JField
+  val elements = (json).children
+
+  //println(elements)
+
+  abstract class DocumentJSON
+  case class ListTy(body: List[Any]) extends DocumentJSON
+  case class JObjTy(body: JObject) extends DocumentJSON
+  case class JstringTy(body: JString) extends DocumentJSON
+  case class JFieldTy(body: JField) extends DocumentJSON
+  case class JArray(body: JArray) extends DocumentJSON
+
+  def paternMatch(elementMatch: DocumentJSON): String = {
+    elementMatch match {
+      case ListTy(body)    => s"List : " + {for { valueList <- body } {
+        if(valueList.isInstanceOf[JObject]){println(valueList); paternMatch(new JObjTy(valueList))}
+        else if(valueList.isInstanceOf[JString]){paternMatch(new JstringTy(valueList))}
+        else if(valueList.isInstanceOf[JField]){paternMatch(new JFieldTy(valueList))}
+        else if(valueList.isInstanceOf[JArray]){paternMatch(new JArray(valueList))}
+      }}
+      case JObjTy(body)    => s"JObj"
+      case JstringTy(body) => s"String"
+      case JFieldTy(body)  => s"Field"
+      case JArray(body)    => s"Array"
+    }
+  }
+
+  val someDoc = ListTy(elements)
+
+  println(paternMatch(someDoc))
+
+
+}
+
