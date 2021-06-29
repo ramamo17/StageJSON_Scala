3d29a8f6e7e71f47787a48073f3c97ad2e6d87c3
Baptiste Lefort <71429321+blefo@users.noreply.github.com>
2021-06-29 15:20:37 +0200

    Update ParsJSON.scala

--- ParsJSON.scala
+++ ParsJSON.scala
@@ -4,7 +4,7 @@ import net.liftweb.json._
 object ParseJsonArray extends App {
   implicit val formats = DefaultFormats
 // a JSON string that represents a list of EmailAccount instances
-  val jsonString = """
+  val json = """
 {
   "$id": "https://example.com/geographical-location.schema.json",
   "$schema": "https://json-schema.org/draft/2020-12/schema",
@@ -27,39 +27,28 @@ object ParseJsonArray extends App {
 }
 """
 
-  val json = parse(jsonString) // on doit itt sur les JField
-  val elements = (json).children
-
-  //println(elements)
-
-  abstract class DocumentJSON
-  case class ListTy(body: List[Any]) extends DocumentJSON
-  case class JObjTy(body: JObject) extends DocumentJSON
-  case class JStringTy(body: JString) extends DocumentJSON
-  case class JFieldTy(body: JField) extends DocumentJSON
-  case class JArrayTy(body: JArray) extends DocumentJSON
-  case class JIntTy(body: JInt) extends DocumentJSON
-
-  def paternMatch(elementMatch: DocumentJSON): String = {
-    elementMatch match {
-      case ListTy(body)    => s"List : " + {for { valueList <- body } {
-        if(valueList.isInstanceOf[JObject]){print(paternMatch(new JObjTy(valueList.asInstanceOf[JObject])))}
-        else if(valueList.isInstanceOf[JString]){print(paternMatch(new JStringTy(valueList.asInstanceOf[JString])))}
-        else if(valueList.isInstanceOf[JField]){print(paternMatch(new JFieldTy(valueList.asInstanceOf[JField])))}
-        else if(valueList.isInstanceOf[JArray]){print(paternMatch(new JArrayTy(valueList.asInstanceOf[JArray])))}
-        else if(valueList.isInstanceOf[JInt]){print(paternMatch(new JIntTy(valueList.asInstanceOf[JInt])))}
-      }}
-      case JObjTy(body)    => s"Object : " + println("    " + paternMatch(new ListTy(body.children)))
-      case JStringTy(body) => s"String : " + println("    " + body.extract[String])
-      case JFieldTy(body)  => s"Field : " + println(body.name + " " + body.value)
-      case JArrayTy(body)  => s"Array : " + println("   " + paternMatch(new ListTy(body.children)))
-      case JIntTy(body)    => s"Int : " + println(body.values)
-    }
+  var jsonParse = json.asInstanceOf[JObject].values;
+  
+  def functionUnList(listPara:List[Any]) : Unit = {
+    listPara.foreach {
+        case value => {
+          if(value.isInstanceOf[Map[String, Any]]){println("Object : " + "   " );functionUnMap(value.asInstanceOf[Map[String, Any]])}
+          else if(value.isInstanceOf[List[Any]]){println("    Array : ");functionUnList(value.asInstanceOf[List[Any]])}
+          else{println("String : " + value)}
+        }
+      }
   }
-
-  val someDoc = ListTy(elements)
-
-  print(paternMatch(someDoc))
+  
+  def functionUnMap(mapPara:Map[String, Any]) : Unit = {
+      mapPara.foreach {
+        case (key, value) => {
+          if(value.isInstanceOf[Map[String, Any]]){println("Object : " + key + "   " );functionUnMap(value.asInstanceOf[Map[String, Any]])}
+          else if(value.isInstanceOf[List[Any]]){println("Array : " + key);functionUnList(value.asInstanceOf[List[Any]])}
+          else{println("String : " + key)}
+        }
+      }
+  }
+  functionUnMap(jsonParse)
 
 
 }
